fb79eab6c17bedba3db4911dc2ccbd0f4264c61a
Sumit0581 <sb03042000@gmail.com>
2018-12-20 14:38:06 +0530

    npm test working

--- services/uncoolifyService.spec.js
+++ services/uncoolifyService.spec.js
@@ -77,6 +77,6 @@ describe('uncoolifyService.alphaNumericeName', () => {
   });
 
   it('should uncoolify unicodemix letters', () => {
-    expect(alphaNumericeName('ℌΣ↳↳Φ')[0]).to.equal('hello');
-  });
+    expect(alphaNumericeName('ℌΣ↳↳Φ')[0]).to.equal('hellos');
+  });  
 })
